b2cc3a69a1f53640c34f968e2f55b5d3f576f471
Khaled Dev Fs <khaled.alhajali91@gmail.com>
2022-01-27 03:20:25 +0100

    projet 1

---
+++ app/src/main/java/com/example/openclasseromeprojet/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.openclasseromeprojet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText mEditTe1;
+    private EditText mEditTe2;
+    private Button mBut1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mEditTe1 = findViewById(R.id.main_editTe1);
+        mEditTe2 = findViewById(R.id.main_editTe2);
+        mBut1 = findViewById(R.id.main_but1);
+        mBut1.setEnabled(false);
+
+        mEditTe1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s1) {
+                mBut1.setEnabled(!s1.toString().isEmpty());
+
+            }
+        });
+
+        mEditTe2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s2) {
+                mBut1.setEnabled(!s2.toString().isEmpty());
+
+            }
+        });
+
+        mBut1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEditTe1.getText().toString();
+                String passWord = mEditTe2.getText().toString();
+                if (email.equals("admin@admin.com") && passWord.equals("admin")){
+                    Intent welcomeActivityIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+                    startActivity(welcomeActivityIntent);
+                }else {
+                    Toast.makeText(MainActivity.this, "Email ou Mot de Passe Invalide", Toast.LENGTH_LONG).show();
+                }
+
+
+
+                }
+
+        });
+    }
+
+
+
+
+
+
+
+
+}
